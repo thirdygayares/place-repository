0eebdbc31fabdf138d38a154125049fa59eb8791
Thirdy Gayares <gayaresthird@gmail.com>
2025-07-25 19:26:32 +0800

    tech: add Dockerfile, database migration scripts, and entrypoint for FastAPI application

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.settings import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
